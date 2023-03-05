ddcd8936f22dd9e48ee947b58c098d22e02baf77
Marek Heintzel <marek.h@automa.net>
2023-03-05 15:31:36 +0100

    feat(header): add jeju font, set up gradients, logo and menu

--- web/app/themes/twin-tails/resources/views/sections/header.blade.php
+++ web/app/themes/twin-tails/resources/views/sections/header.blade.php
@@ -1,11 +1,37 @@
-<header class="banner h-header-height">
-  <a class="brand font-another-hand" href="{{ home_url('/') }}">
+<header
+  class="border-bottom-gradient
+  border-b-15
+  max-w-container
+  h-header-height
+  flex
+  justify-center
+  gap-36
+   items-end">
+
+  <a class="
+  text-gradient
+  brand
+  font-another-hand
+  text-9xl " href="{{ home_url('/') }}">
     {!! $siteName !!}
   </a>
 
   @if (has_nav_menu('primary_navigation'))
-    <nav class="nav-primary" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
-      {!! wp_nav_menu(['theme_location' => 'primary_navigation', 'menu_class' => 'nav', 'echo' => false]) !!}
+    <nav class="nav-primary mb-8" aria-label="{{ wp_get_nav_menu_name('primary_navigation') }}">
+      {!!
+    wp_nav_menu(
+      [
+      'theme_location' => 'primary_navigation',
+      'menu_class' =>
+        'flex
+        gap-20
+        font-jeju-gothic
+        text-3xl
+        lowercase',
+      'echo' => false,
+      ])
+        !!}
     </nav>
   @endif
+
 </header>
--- web/app/themes/twin-tails/tailwind.config.cjs
+++ web/app/themes/twin-tails/tailwind.config.cjs
@@ -4,12 +4,19 @@ module.exports = {
   theme: {
     fontFamily : {
       'another-hand' : ['Just Another Hand', 'cursive'],
+      'jeju-gothic' : ['JejuGothic', 'sans-serif'],
+    },
+    borderWidth: {
+      '15': '15px',
     },
     extend: {
       colors: {}, // Extend Tailwind's default colors
       spacing: {
         'header-height': 'var(--header-height)',
       },
+      maxWidth: {
+        'container' : 'var(--container-width)',
+      }
     },
   },
   plugins: [],
