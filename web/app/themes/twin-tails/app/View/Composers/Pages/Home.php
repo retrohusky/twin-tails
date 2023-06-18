9a95e53a7d6bebb09413a56aaf5f959978216ae6
Marek Heintzel <marek.h@automa.net>
2023-06-18 13:27:17 +0200

    feat: video on home

--- web/app/themes/twin-tails/app/View/Composers/Pages/Home.php
+++ web/app/themes/twin-tails/app/View/Composers/Pages/Home.php
@@ -2,6 +2,7 @@
 
 namespace App\View\Composers\Pages;
 
+use Illuminate\Support\Arr;
 use Roots\Acorn\View\Composer;
 
 class Home extends Composer
@@ -14,6 +15,8 @@ class Home extends Composer
     {
         return [
             'lastChapter' => $this->lastChapter(),
+            'firstChapter' => $this->firstChapter(),
+            'mainVideo' => $this->mainVideo(),
         ];
     }
 
@@ -27,6 +30,34 @@ class Home extends Composer
             'order' => 'DESC',
         ]);
 
-        return $lastChapter[0];
+        return Arr::get($lastChapter, 0, null);
+    }
+
+    private function firstChapter()
+    {
+        $firstChapter = get_posts([
+            'post_type' => 'chapter',
+            'posts_per_page' => 1,
+            'orderby' => 'date',
+            'post_status' => 'publish',
+            'order' => 'ASC',
+        ]);
+
+        return Arr::get($firstChapter, 0, null);
+    }
+
+    private function mainVideo()
+    {
+        if ( !function_exists( 'get_field' ) ) {
+            return null;
+        }
+
+        $video = get_field('home_video');
+
+        if ( !$video ) {
+            return null;
+        }
+
+        return $video;
     }
 }
--- web/app/themes/twin-tails/resources/views/template-home.blade.php
+++ web/app/themes/twin-tails/resources/views/template-home.blade.php
@@ -4,17 +4,25 @@
 
 @php
 /** @var WP_Post|null $lastChapter */
+/** @var WP_Post|null $firstChapter */
+/** @var string $mainVideo */
 @endphp
 
 @extends('layouts.app')
 
 @section('content')
-  @if ($lastChapter)
 
     <div class="tt-home">
+
+      @if($mainVideo)
+        <div class="tt-home__video">
+          <iframe src="{{ $mainVideo }}" allowfullscreen></iframe>
+        </div>
+      @endif
+
       <div class="tt-home__chapters">
         <a class="tt-btn tt-btn--special"
-          href="#">First Chapter</a>
+          href="{{ get_permalink($firstChapter) }}">First Chapter</a>
         <a class="tt-btn tt-btn--special"
            href="{{ get_permalink($lastChapter) }}">Last Chapter</a>
       </div>
@@ -24,10 +32,9 @@
       </div>
 
       <div>
-        <a class="tt-btn tt-btn--special"
-          href="">Want more? Check our media section...</a>
+        <p>
+          Want more? Check our <a class="h2" href="#">media section</a>
+        </p>
       </div>
     </div>
-
-  @endif
 @endsection
